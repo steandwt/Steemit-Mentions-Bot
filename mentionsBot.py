018c50948b7db3148bea4c412fe7a9da260ddc21
Stéan de Wit <stean.dewit1@gmail.com>
2017-11-02 12:15:11 +0200

    Update mentionsBot.py

--- mentionsBot.py
+++ mentionsBot.py
@@ -2,7 +2,7 @@ from steem import Steem
 from steem.blockchain import Blockchain
 from steem.post import Post
 
-#crate steem instance and pass it our key
+#crate steem instance and pass it your key
 s = Steem(keys = ["<your private posting key>"])
 #create blockchain instance
 b = Blockchain()
@@ -17,12 +17,14 @@ for post in stream:
         users = post.json_metadata.get('users', [])
         #check if we are tagged
         if "<your-steemit-username-in-lowercase>" in users:
-            #if we are tagged we'll upvote the post/comment with 10% voteweight
+            #if we are tagged we'll upvote the post/comment with 10% voteweight. You
+            #can change the vote accordingly
             post.vote(10.0, "<your steemit username>")
 
         else:
-            #we are not tagged so we will skip this post
+            #we are not tagged so we'll skip this post
             pass
-
+        
+        #Catch any unexpected exceptions
     except Exception as e:
         print("Error: "+str(e))
